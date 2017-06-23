440675c98bdc7d5d965a2c05f4f82e7172734854
Thyerry <Thyerry@Thyerry-PC>
2017-06-23 16:38:42 -0300

    teste

--- src/br/ufrpe/bugzilla/negocio/beans/Rastreio.java
+++ src/br/ufrpe/bugzilla/negocio/beans/Rastreio.java
@@ -1,5 +1,10 @@
 package br.ufrpe.bugzilla.negocio.beans;
 
+import java.time.LocalDateTime;
+
 public class Rastreio {
+	private String situacao;
+	private Endereco local;
+	private LocalDateTime dataHora;
 
 }
