91e9299751bc3a9534282077aa898176a1b6ca07
Diego Patrik <dpatrikone@gmail.com>
2017-06-25 22:49:35 -0300

    beans local

--- src/br/ufrpe/bugzilla/negocio/beans/Local.java
+++ src/br/ufrpe/bugzilla/negocio/beans/Local.java
@@ -1,5 +1,35 @@
 package br.ufrpe.bugzilla.negocio.beans;
 
 public class Local {
+	
+	private String nome;
+	//vai ser utilizada para calcular tarifa e tempo (distancia em km da central)
+	private int localizacao;
+	
+	Local(String nome, int localizacao){
+		
+		this.nome = nome;
+		this.localizacao = localizacao;
+	}
+	
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+	
+	public String getNome(){
+		return this.nome;
+	}
 
+	public int getLocalizacao() {
+		return localizacao;
+	}
+
+	public void setLocalizacao(int localizacao) {
+		this.localizacao = localizacao;
+	}
+
+	public String toString() {
+		return "Local: " + nome + ", Localização=" + localizacao;
+	}
+	
 }
