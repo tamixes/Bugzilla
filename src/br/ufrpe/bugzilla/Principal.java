8709fa638f6720a41c288a777d86be0d90250558
Diego Patrik <dpatrikone@gmail.com>
2017-08-01 12:05:20 -0300

    removi teste local

--- src/br/ufrpe/bugzilla/Principal.java
+++ src/br/ufrpe/bugzilla/Principal.java
@@ -1,17 +1,14 @@
 package br.ufrpe.bugzilla;
 
 import br.ufrpe.bugzilla.gui.TelaTextual;
-import br.ufrpe.bugzilla.gui.TesteLocal;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		/*TelaTextual tela = new TelaTextual();
-		tela.MenuPrincipal();*/
+		TelaTextual tela = new TelaTextual();
+		tela.MenuPrincipal();
 		
-		TesteLocal teste = new TesteLocal();
-		teste.Menu();
 	}
 
 }
