4baef14b44000e965908ab585d888bf720ffd5fc
Diego Patrik <dpatrikone@gmail.com>
2017-08-14 23:31:41 -0300

    pdf com img

--- src/br/ufrpe/bugzilla/negocio/beans/GeraPDF.java
+++ src/br/ufrpe/bugzilla/negocio/beans/GeraPDF.java
@@ -5,9 +5,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
@@ -22,6 +24,9 @@ public class GeraPDF {
 			PdfWriter.getInstance(documento, new FileOutputStream("encomenda.pdf"));
 			
 			documento.open();
+			Image img = Image.getInstance("logo2_bugzilla.png");
+			documento.add(img);
+			documento.add(new Paragraph ("\n\n\n"));
 			documento.add(new Paragraph("Informações da Encomenda " + enc.getCodigo() + "\n\n\n\n"));
 			documento.add(new Paragraph(enc.toString()+ "\n\n"));
 			
@@ -29,6 +34,10 @@ public class GeraPDF {
 			System.out.println(e.getMessage());
 		}catch(FileNotFoundException e){
 			System.out.println(e.getMessage());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}finally{
 			documento.close();
 		}
